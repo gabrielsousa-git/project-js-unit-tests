78f846ded4d537c68c4ce513e6d90368fa83fe8f
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2023-01-23 16:40:28 -0300

    Iniciando implementação do req 10

--- src/restaurant.js
+++ src/restaurant.js
@@ -93,6 +93,13 @@
 // - retornará o valor somado acrescido de 10%.
 // DICA: para isso, você precisará percorrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const createMenu = () => {};
+const createMenu = () => {
+  const object = {};
+  return {
+    fetchMenu: () => {
+      
+    },
+  };
+};
 
 module.exports = createMenu;
